611fb75e97d46a9fe6bd58522e4bd95ff25822b1
A2saur <a2_shop@atsaur.com>
2019-09-08 15:37:32 -0400

    Initialization

--- src/com/atsaur/Hello_World.java
+++ src/com/atsaur/Hello_World.java
@@ -5,6 +5,5 @@ class Hello_World {
     Hello_World() {
         // write your code here
         System.out.println("Hello World! (from the constructor)");
-        System.out.println("Testing GIT");
     }
 }
